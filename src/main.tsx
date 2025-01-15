1bc4aa3d4e29e44e040e2fd1fd132af9a8096e54
nekit <nick.misiura@yandex.ru>
2025-01-15 20:39:31 +0300

    add routes for /products and /cart pages

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,8 @@ createRoot(document.getElementById('root')!).render(
   <Routes>
     <Route element={<App />} >
       <Route path='/' element={<div>Home</div>} />
+      <Route path='/products' element={<div>Products Page</div>} />
+      <Route path='/cart' element={<div>Cart Page</div>} />
     </Route>
   </Routes>
 </BrowserRouter>
