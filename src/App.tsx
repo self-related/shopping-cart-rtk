5c4a7a890f8f33a3d1da1a3fc9fc08a610ea3cdf
nekit <nick.misiura@yandex.ru>
2025-01-16 19:09:22 +0300

    Add daisyui dev dependency; Add and customize daisyui's navbar with icon, indicator and dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <div className='bg-white'>
     <Navbar />
       <main className='max-w-7xl mx-auto min-h-screen'>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 }
 
