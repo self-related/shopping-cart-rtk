73684c4fac5410c019c966c83d4a0605076afd00
nekit <nick.misiura@yandex.ru>
2025-01-16 16:51:28 +0300

    add Navbar component, lil style for <main> section in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router';
 import './App.css'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>Navbar</nav>
-      <main>
+    <Navbar />
+      <main className='max-w-7xl mx-auto min-h-screen'>
         <Outlet />
       </main>
     </>
