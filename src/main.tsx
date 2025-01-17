83472a591ce7c59d0dd7d82c52c7a6350aa3dc4a
nekit <nick.misiura@yandex.ru>
2025-01-17 16:13:37 +0300

    Create an empty store; Wrap components in main.tsx with redux's Provider of the empty store

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-<BrowserRouter>
-  <Routes>
-    <Route element={<App />} >
-      <Route path='/' element={<div>Home</div>} />
-      <Route path='/products' element={<div>Products Page</div>} />
-      <Route path='/cart' element={<div>Cart Page</div>} />
-    </Route>
-  </Routes>
-</BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />} >
+          <Route path='/' element={<div>Home</div>} />
+          <Route path='/products' element={<div>Products Page</div>} />
+          <Route path='/cart' element={<div>Cart Page</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
