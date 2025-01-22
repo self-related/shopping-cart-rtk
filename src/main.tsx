3a978cd0bedd73f8be30eaecfeb58f19b52db76d
Nekit <nick.misiura@yandex.ru>
2025-01-22 17:32:18 +0300

    add a home page

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,14 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
+import Home from './redux/features/products/pages/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route element={<App />} >
-          <Route path='/' element={<div>Home</div>} />
+          <Route path='/' element={<Home />} />
           <Route path='/products' element={<div>Products Page</div>} />
           <Route path='/cart' element={<div>Cart Page</div>} />
         </Route>
---
+++ src/redux/features/products/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+        <div className="py-8 px-4 max-w-7xl">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8        ">
+                <div className="col-span-2">Display Products</div>
+                <div>Add new product</div>
+            </div>
+        </div>
+    );
+}
