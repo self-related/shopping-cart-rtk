9cdb3b2bfd39e60a66f56a8953dedccec6a2800a
Nekit <nick.misiura@yandex.ru>
2025-01-22 17:36:41 +0300

    get access to the product slice's state

--- src/redux/features/products/pages/Home.tsx
+++ src/redux/features/products/pages/Home.tsx
@@ -1,5 +1,10 @@
+import { useAppSelector } from "../../../store";
 
 export default function Home() {
+    const products = useAppSelector((state) => state.products);
+    console.log(products);
+
+    
     return (
         <div className="py-8 px-4 max-w-7xl">
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-8        ">
