6f19f8b3d0d72f6338ea723b2fc71c298297fcf9
Nekit <nick.misiura@yandex.ru>
2025-01-28 12:57:46 +0300

    fix wrong page directory location

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './redux/store.ts'
-import Home from './redux/features/products/pages/Home.tsx'
+import Home from './pages/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
