3a2dbc608dc99d2729161e7805e033683de43914
Nekit <nick.misiura@yandex.ru>
2025-01-30 13:58:04 +0300

    add form change handling

--- src/pages/products/AddProduct.tsx
+++ src/pages/products/AddProduct.tsx
@@ -1,16 +1,36 @@
+import React, { useState } from "react";
+
 export default function AddProduct() {
+
+    const [name, setName] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
+    const [image, setImage] = useState<string>("");
+    const [price, setPrice] = useState<string>("");
+    const [date, setDate] = useState<string>("");
+
+    type EventElement = HTMLInputElement | HTMLSelectElement;
+
+    const handleInput = (event: React.FormEvent<EventElement>, setState: React.Dispatch<React.SetStateAction<string>>) => {
+        setState(event.currentTarget.value);
+    };
+
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    };
+
+    console.log({name, category, image, price, date});
 
     return (
         <div className="max-w-md bg-white shadow-lg rounded-lg p-6 h-fit align-middle">
             <h2 className="text-md font-semibold my-4 text-center">Add a new product</h2>
-            <form className="space-y-4">
+            <form onSubmit={event => handleFormSubmit(event)} className="space-y-4">
 
                 {/* product name */}
                 <div>
                     <label htmlFor="" className="block text-sm font-medium text-gray-600">
                         Product name: 
                     </label>
-                    <input type="text" name="name" id="name" placeholder="Insert a product name" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
+                    <input onInput={event => handleInput(event, setName)} value={name} type="text" name="name" id="name" required placeholder="Insert a product name" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
                 </div>
 
                 {/* category */}
@@ -18,7 +38,7 @@ export default function AddProduct() {
                     <label htmlFor="" className="block text-sm font-medium text-gray-600">
                         Category: 
                     </label>
-                    <select name="category" id="category" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500">
+                    <select onInput={event => handleInput(event, setCategory)} name="category" id="category" required className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500">
                         <option value="">Choose a category</option>
                         <option value="clothing">Clothing</option>
                         <option value="gadgets">Gadgets</option>
@@ -29,9 +49,9 @@ export default function AddProduct() {
                 {/* image url */}
                 <div>
                     <label htmlFor="" className="block text-sm font-medium text-gray-600">
-                        Product name: 
+                        Image: 
                     </label>
-                    <input type="text" name="image" id="image" placeholder="Insert image URL" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
+                    <input onInput={event => handleInput(event, setImage)} type="text" name="image" id="image" placeholder="Insert image URL" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
                 </div>
 
                 {/* price and upload date*/}
@@ -40,14 +60,14 @@ export default function AddProduct() {
                         <label htmlFor="" className="block text-sm font-medium text-gray-600">
                             Price: 
                         </label>
-                        <input type="number" name="price" id="price" placeholder="$" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
+                        <input onInput={event => handleInput(event, setPrice)} value={price} type="number" required name="price" id="price" placeholder="$" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
                     </div>
 
                     <div>
                         <label htmlFor="" className="block text-sm font-medium text-gray-600">
                             Date: 
                         </label>
-                        <input type="date" name="date" id="date" className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
+                        <input onInput={event => handleInput(event, setDate)} type="date" name="date" id="date" required className="bg-gray-50 w-full text-black mt-1 p-2 rounded-lg border-[2.3px] focus:outline-none focus:border-[2.7px] focus:border-indigo-500"/>
                     </div>
                 </div>
 
