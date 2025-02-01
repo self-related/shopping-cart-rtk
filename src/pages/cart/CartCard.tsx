e77f9ae786eed8cf0a8adca9e909d2edd365892f
Nekit <nick.misiura@yandex.ru>
2025-02-01 13:12:42 +0300

    add a card for products in cart, dispatch removeFromCart action on click

---
+++ src/pages/cart/CartCard.tsx
@@ -0,0 +1,46 @@
+import { useDispatch } from "react-redux";
+import { Product } from "../../types";
+
+interface ProductCardProps {
+    product: Product
+}
+
+export default function CartCard( {product}: ProductCardProps) {
+
+    const { id, name, date, image, price, description } = product ?? null;
+    if (product == null) {
+        console.log("Not provided");
+    }
+
+    const dispatch = useDispatch()
+
+    const handleRemoveProduct = () => {
+        dispatch({type: "cartSlice/removeFromCart", payload: id});
+    };
+
+    return (
+        <div className="card bg-base-100 w-96 shadow-xl">
+            <figure>
+                <img
+                    src={image}
+                    alt={name} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title text-white">
+                    {name}
+                </h2>
+                <p>
+                    {description ?? "No description"}
+                </p>
+                <div className="card-actions justify-end items-center">
+                    <p className="text-gray-200 font-semibold">
+                        ${price}
+                    </p>
+                    <button onClick={handleRemoveProduct} className="btn btn-primary bg-red-500 text-white hover:bg-red-700 border-none" >
+                        Remove
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
