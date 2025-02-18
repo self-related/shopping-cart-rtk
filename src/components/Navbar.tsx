3afd1aaeafca4f7de4617f8dd51936b766a3b59d
Nekit <nick.misiura@yandex.ru>
2025-02-18 20:32:05 +0300

    fixed wrong cart items quantity on the navbar (again)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
                                         strokeWidth="2"
                                         d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                 </svg>
-                                <span className="badge badge-sm indicator-item">{cartProducts.length}</span>
+                                <span className="badge badge-sm indicator-item">{cartTotalProducts}</span>
                             </div>
                         </div>
 
@@ -44,7 +44,7 @@ export default function Navbar() {
                             tabIndex={0}
                             className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
                             <div className="card-body">
-                                <span className="text-lg font-bold">{cartProducts.length} Items</span>
+                                <span className="text-lg font-bold">{cartTotalProducts} Items</span>
                                 <span className="text-info">Subtotal: ${cartTotalPrice}</span>
                                 <div className="card-actions">
                                     <Link to="/cart">
